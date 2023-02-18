5cfcb9389bc1a9f01cb3e30b5a154ff73b6fc5ed
Lam <maiphuonglambh.2002@gmail.com>
2023-02-18 20:21:29 +0700

    end

---
+++ server/src/controllers/createCardController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import Deck from '../models/Deck'
+
+
+export const createCardController = async (req: Request, res: Response) => {
+    const deckId = req.params.deckId;
+    const deck = await Deck.findById(deckId);
+    if (!deck) return res.status(400).send("no deck of this id exists");
+    const { text } = req.body;
+    deck.cards.push(text);
+    await deck.save();
+    res.json(deck);
+}
